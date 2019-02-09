61463c6528832dabdd022bca6f288558adf3f4d9
TRAN MINH DAT <1512103@student.hcmus.edu.vn>
2019-02-09 17:48:33 +0700

    fix

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ Created on Sat Feb  9 17:15:32 2019
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-from scipy.stats import norm
-from sklearn.preprocessing import StandardScaler
-from scipy import stats
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#import numpy as np
+#from scipy.stats import norm
+#from sklearn.preprocessing import StandardScaler
+#from scipy import stats
 import warnings
 warnings.filterwarnings('ignore')
 
