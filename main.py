634844d2057f41840dd7eee8777d991ba93ff11b
minhdat97 <minhdat.tran97@gmail.com>
2019-02-09 17:25:25 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  9 17:15:32 2019
+
+@author: TRANMINHDAT
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+
+df_train = pd.read_csv('../input/train.csv')
+
+df_train.columns
